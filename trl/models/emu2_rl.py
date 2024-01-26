f815bcb99f9b45f86d779501d0e1992e1858d9dd
pkuanjie <pkuanjie@gmail.com>
2024-01-26 13:57:04 -0500

    add emu_rl class

---
+++ trl/models/emu2_rl.py
@@ -0,0 +1,429 @@
+from dataclasses import dataclass
+import inspect
+from typing import List, Optional
+
+from PIL import Image
+import numpy as np
+from tqdm import tqdm
+
+from diffusers import DiffusionPipeline
+from diffusers.utils import BaseOutput
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+EVA_IMAGE_SIZE = 448
+OPENAI_DATASET_MEAN = (0.48145466, 0.4578275, 0.40821073)
+OPENAI_DATASET_STD = (0.26862954, 0.26130258, 0.27577711)
+DEFAULT_IMG_PLACEHOLDER = "[<IMG_PLH>]"
+
+
+@dataclass
+class EmuVisualGenerationPipelineOutput(BaseOutput):
+    image: Image.Image
+    nsfw_content_detected: Optional[bool]
+
+
+class EmuRL:
+    _callback_tensor_inputs = ["latents", "prompt_embeds", "negative_prompt_embeds"]
+
+    def __init__(self, path, torch_dtype=torch.bfloat16, variant="bf16", use_safetensors=True) -> None:
+        # save parameter for creating the emu model
+        self.path = path
+        self.variant = variant
+        self.use_safetensors = use_safetensors
+        self.torch_dtype = torch_dtype
+
+        # load the emu model from the huggingface checkpoint
+        self.multimodal_encoder = AutoModelForCausalLM.from_pretrained(
+            f"{self.path}/multimodal_encoder",
+            trust_remote_code=True,
+            torch_dtype=self.torch_dtype,
+            use_safetensors=self.use_safetensors,
+            variant=self.variant,
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(f"{self.path}/tokenizer")
+
+        self.emu_pipeline = DiffusionPipeline.from_pretrained(
+            self.path,
+            custom_pipeline="pipeline_emu2_gen",
+            torch_dtype=self.torch_dtype,
+            use_safetensors=self.use_safetensors,
+            variant=self.variant,
+            multimodal_encoder=self.multimodal_encoder,
+            tokenizer=self.tokenizer,
+        )
+
+        # link all modules of the emu model to this class. This class becomes a puppet of emu with added functioins for RL
+        # this transformation is to make the emu model more suitable for RL training (with trl) but keep its original model and class intact
+        self.multimodal_encoder = self.emu_pipeline.multimodal_encoder
+        self.tokenizer = self.emu_pipeline.tokenizer
+        self.unet = self.emu_pipeline.unet
+        self.scheduler = self.emu_pipeline.scheduler
+        self.vae = self.emu_pipeline.vae
+        self.feature_extractor = self.emu_pipeline.feature_extractor
+        self.safety_checker = self.emu_pipeline.safety_checker
+        self.vae_scale_factor = self.emu_pipeline.vae_scale_factor
+        self.transform = self.emu_pipeline.transform
+        self.negative_prompt = self.emu_pipeline.negative_prompt
+        self.image_processor = self.emu_pipeline.image_processor
+
+    def device(self, module=None):
+        if module is None:
+            return next(self.emu_pipeline.parameters()).device
+        return next(module.parameters()).device
+
+    def dtype(self, module=None):
+        if module is None:
+            return next(self.emu_pipeline.parameters()).dtype
+        return next(module.parameters()).dtype
+
+    # newly added for ddpo
+    def check_inputs(
+        self,
+        prompt,
+        height,
+        width,
+        callback_steps,
+        negative_prompt=None,
+        prompt_embeds=None,
+        negative_prompt_embeds=None,
+        callback_on_step_end_tensor_inputs=None,
+    ):
+        if height % 8 != 0 or width % 8 != 0:
+            raise ValueError(f"`height` and `width` have to be divisible by 8 but are {height} and {width}.")
+
+        if callback_steps is not None and (not isinstance(callback_steps, int) or callback_steps <= 0):
+            raise ValueError(
+                f"`callback_steps` has to be a positive integer but is {callback_steps} of type"
+                f" {type(callback_steps)}."
+            )
+        if callback_on_step_end_tensor_inputs is not None and not all(
+            k in self._callback_tensor_inputs for k in callback_on_step_end_tensor_inputs
+        ):
+            raise ValueError(
+                f"`callback_on_step_end_tensor_inputs` has to be in {self._callback_tensor_inputs}, but found {[k for k in callback_on_step_end_tensor_inputs if k not in self._callback_tensor_inputs]}"
+            )
+
+        if prompt is not None and prompt_embeds is not None:
+            raise ValueError(
+                f"Cannot forward both `prompt`: {prompt} and `prompt_embeds`: {prompt_embeds}. Please make sure to"
+                " only forward one of the two."
+            )
+        elif prompt is None and prompt_embeds is None:
+            raise ValueError(
+                "Provide either `prompt` or `prompt_embeds`. Cannot leave both `prompt` and `prompt_embeds` undefined."
+            )
+        elif prompt is not None and (not isinstance(prompt, str) and not isinstance(prompt, list)):
+            raise ValueError(f"`prompt` has to be of type `str` or `list` but is {type(prompt)}")
+
+        if negative_prompt is not None and negative_prompt_embeds is not None:
+            raise ValueError(
+                f"Cannot forward both `negative_prompt`: {negative_prompt} and `negative_prompt_embeds`:"
+                f" {negative_prompt_embeds}. Please make sure to only forward one of the two."
+            )
+
+        if prompt_embeds is not None and negative_prompt_embeds is not None:
+            if prompt_embeds.shape != negative_prompt_embeds.shape:
+                raise ValueError(
+                    "`prompt_embeds` and `negative_prompt_embeds` must have the same shape when passed directly, but"
+                    f" got: `prompt_embeds` {prompt_embeds.shape} != `negative_prompt_embeds`"
+                    f" {negative_prompt_embeds.shape}."
+                )
+
+    # newly added for ddpo
+    @property
+    def _execution_device(self):
+        return self.device(self.unet)
+
+    # newly added for ddpo
+    def prepare_extra_step_kwargs(self, generator, eta):
+        # prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
+        # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
+        # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
+        # and should be between [0, 1]
+
+        accepts_eta = "eta" in set(inspect.signature(self.scheduler.step).parameters.keys())
+        extra_step_kwargs = {}
+        if accepts_eta:
+            extra_step_kwargs["eta"] = eta
+
+        # check if the scheduler accepts generator
+        accepts_generator = "generator" in set(inspect.signature(self.scheduler.step).parameters.keys())
+        if accepts_generator:
+            extra_step_kwargs["generator"] = generator
+        return extra_step_kwargs
+
+    # newly added for ddpo
+    def prepare_latents(
+        self,
+        batch_size,
+        num_channels_latents,
+        height,
+        width,
+        dtype,
+        device,
+        generator,
+        latents=None,
+    ):
+        shape = (
+            batch_size,
+            num_channels_latents,
+            height // self.vae_scale_factor,
+            width // self.vae_scale_factor,
+        )
+        if isinstance(generator, list) and len(generator) != batch_size:
+            raise ValueError(
+                f"You have passed a list of generators of length {len(generator)}, but requested an effective batch"
+                f" size of {batch_size}. Make sure the batch size matches the length of the generators."
+            )
+
+        if latents is None:
+            latents = torch.randn(shape, device=device, dtype=dtype, generator=generator)
+        else:
+            latents = latents.to(device)
+
+        # scale the initial noise by the standard deviation required by the scheduler
+        latents = latents * self.scheduler.init_noise_sigma
+        return latents
+
+    # newly added for ddpo
+    def _encode_prompt(
+        self,
+        device,
+        num_images_per_prompt,
+        do_classifier_free_guidance,
+        prompt_embeds,
+        negative_prompt_embeds,
+        prompt,  # a dummy argument
+        negative_prompt,  # a dummy argument
+        lora_scale=None,  # a dummy argument
+    ):
+        r"""
+        Encodes the prompt into text encoder hidden states.
+
+        Args:
+            prompt (`str` or `List[str]`, *optional*):
+                prompt to be encoded
+            device: (`torch.device`):
+                torch device
+            num_images_per_prompt (`int`):
+                number of images that should be generated per prompt
+            do_classifier_free_guidance (`bool`):
+                whether to use classifier free guidance or not
+            negative_prompt (`str` or `List[str]`, *optional*):
+                The prompt or prompts not to guide the image generation. If not defined, one has to pass
+                `negative_prompt_embeds` instead. Ignored when not using guidance (i.e., ignored if `guidance_scale` is
+                less than `1`).
+            prompt_embeds (`torch.FloatTensor`, *optional*):
+                Pre-generated text embeddings. Can be used to easily tweak text inputs, *e.g.* prompt weighting. If not
+                provided, text embeddings will be generated from `prompt` input argument.
+            negative_prompt_embeds (`torch.FloatTensor`, *optional*):
+                Pre-generated negative text embeddings. Can be used to easily tweak text inputs, *e.g.* prompt
+                weighting. If not provided, negative_prompt_embeds will be generated from `negative_prompt` input
+                argument.
+            lora_scale (`float`, *optional*):
+                A LoRA scale that will be applied to all LoRA layers of the text encoder if LoRA layers are loaded.
+            clip_skip (`int`, *optional*):
+                Number of layers to be skipped from CLIP while computing the prompt embeddings. A value of 1 means that
+                the output of the pre-final layer will be used for computing the prompt embeddings.
+        """
+        batch_size = prompt_embeds.shape[0]
+
+        if self.unet is not None:
+            prompt_embeds_dtype = self.unet.dtype
+        else:
+            prompt_embeds_dtype = prompt_embeds.dtype
+
+        prompt_embeds = prompt_embeds.to(dtype=prompt_embeds_dtype, device=device)
+
+        bs_embed, seq_len, _ = prompt_embeds.shape
+        # duplicate text embeddings for each generation per prompt, using mps friendly method
+        prompt_embeds = prompt_embeds.repeat(1, num_images_per_prompt, 1)
+        prompt_embeds = prompt_embeds.view(bs_embed * num_images_per_prompt, seq_len, -1)
+
+        if do_classifier_free_guidance:
+            # duplicate unconditional embeddings for each generation per prompt, using mps friendly method
+            seq_len = negative_prompt_embeds.shape[1]
+
+            negative_prompt_embeds = negative_prompt_embeds.to(dtype=prompt_embeds_dtype, device=device)
+
+            negative_prompt_embeds = negative_prompt_embeds.repeat(1, num_images_per_prompt, 1)
+            negative_prompt_embeds = negative_prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
+
+        return prompt_embeds, negative_prompt_embeds
+
+    @torch.no_grad()
+    def forward(
+        self,
+        inputs: List[Image.Image | str] | str | Image.Image,
+        height: int = 1024,
+        width: int = 1024,
+        num_inference_steps: int = 50,
+        guidance_scale: float = 3.0,
+        crop_info: List[int] = [0, 0],
+        original_size: List[int] = [1024, 1024],
+    ):
+        if not isinstance(inputs, list):
+            inputs = [inputs]
+
+        # 0. Default height and width to unet
+        height = height or self.unet.config.sample_size * self.vae_scale_factor
+        width = width or self.unet.config.sample_size * self.vae_scale_factor
+
+        device = self.device(self.unet)
+        dtype = self.dtype(self.unet)
+
+        do_classifier_free_guidance = guidance_scale > 1.0
+
+        # 1. Encode input prompt
+        prompt_embeds = self._prepare_and_encode_inputs(
+            inputs,
+            do_classifier_free_guidance,
+        ).to(device=device, dtype=dtype)
+        batch_size = prompt_embeds.shape[0] // 2 if do_classifier_free_guidance else prompt_embeds.shape[0]
+
+        unet_added_conditions = {}
+        time_ids = torch.LongTensor(original_size + crop_info + [height, width]).to(device)
+        if do_classifier_free_guidance:
+            unet_added_conditions["time_ids"] = torch.cat([time_ids, time_ids], dim=0)
+        else:
+            unet_added_conditions["time_ids"] = time_ids
+        unet_added_conditions["text_embeds"] = torch.mean(prompt_embeds, dim=1)
+
+        # 2. Prepare timesteps
+        self.scheduler.set_timesteps(num_inference_steps, device=device)
+        timesteps = self.scheduler.timesteps
+
+        # 3. Prepare latent variables
+        shape = (
+            batch_size,
+            self.unet.config.in_channels,
+            height // self.vae_scale_factor,
+            width // self.vae_scale_factor,
+        )
+        latents = torch.randn(shape, device=device, dtype=dtype)
+        latents = latents * self.scheduler.init_noise_sigma
+
+        # 4. Denoising loop
+        for t in tqdm(timesteps):
+            # expand the latents if we are doing classifier free guidance
+            # 2B x 4 x H x W
+            latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+
+            noise_pred = self.unet(
+                latent_model_input,
+                t,
+                encoder_hidden_states=prompt_embeds,
+                added_cond_kwargs=unet_added_conditions,
+            ).sample
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_cond, noise_pred_uncond = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_cond - noise_pred_uncond)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+
+        # 5. Post-processing
+        images = self.decode_latents(latents)
+
+        # 6. Run safety checker
+        images, has_nsfw_concept = self.run_safety_checker(images, device, dtype)
+
+        # 7. Convert to PIL
+        images = self.numpy_to_pil(images)
+        return EmuVisualGenerationPipelineOutput(
+            image=images[0],
+            nsfw_content_detected=None if has_nsfw_concept is None else has_nsfw_concept[0],
+        )
+
+    def _prepare_and_encode_inputs(
+        self,
+        inputs: List[str | Image.Image],
+        do_classifier_free_guidance: bool = False,
+        placeholder: str = DEFAULT_IMG_PLACEHOLDER,
+    ):
+        device = self.device(self.multimodal_encoder.visual)
+        dtype = self.dtype(self.multimodal_encoder.visual)
+
+        has_image, has_text = False, False
+        text_prompt, image_prompt = "", []
+        for x in inputs:
+            if isinstance(x, str):
+                has_text = True
+                text_prompt += x
+            else:
+                has_image = True
+                text_prompt += placeholder
+                image_prompt.append(self.transform(x))
+
+        if len(image_prompt) == 0:
+            image_prompt = None
+        else:
+            image_prompt = torch.stack(image_prompt)
+            image_prompt = image_prompt.to(device=device, dtype=dtype)
+
+        # Enable Autoencoding Mode, you can ONLY input exactly one image
+        if has_image and not has_text:
+            prompt = self.multimodal_encoder.encode_image(image=image_prompt)
+            if do_classifier_free_guidance:
+                key = "[NULL_IMAGE]"
+                if key not in self.negative_prompt:
+                    negative_image = torch.zeros_like(image_prompt)
+                    self.negative_prompt[key] = self.multimodal_encoder.encode_image(image=negative_image)
+                prompt = torch.cat([prompt, self.negative_prompt[key]], dim=0)
+        # Enable Image Generation Mode
+        else:
+            prompt = self.multimodal_encoder.generate_image(text=[text_prompt], image=image_prompt)
+            if do_classifier_free_guidance:
+                key = ""
+                if key not in self.negative_prompt:
+                    self.negative_prompt[key] = self.multimodal_encoder.generate_image(text=[key])
+                prompt = torch.cat([prompt, self.negative_prompt[key]], dim=0)
+
+        return prompt
+
+    # added for ddpo
+    def _get_negative_prompt_embedding(self, key: str):
+        if key not in self.negative_prompt:
+            self.negative_prompt[key] = self.multimodal_encoder.generate_image(text=[key])
+        return self.negative_prompt[key]
+
+    def decode_latents(self, latents: torch.Tensor) -> np.ndarray:
+        latents = 1 / self.vae.config.scaling_factor * latents
+        image = self.vae.decode(latents).sample
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).float().numpy()
+        return image
+
+    def numpy_to_pil(self, images: np.ndarray) -> List[Image.Image]:
+        """
+        Convert a numpy image or a batch of images to a PIL image.
+        """
+        if images.ndim == 3:
+            images = images[None, ...]
+        images = (images * 255).round().astype("uint8")
+        if images.shape[-1] == 1:
+            # special case for grayscale (single channel) images
+            pil_images = [Image.fromarray(image.squeeze(), mode="L") for image in images]
+        else:
+            pil_images = [Image.fromarray(image) for image in images]
+
+        return pil_images
+
+    def run_safety_checker(
+        self,
+        image: np.ndarray,
+        device: torch.device,
+        dtype: torch.dtype,
+    ):
+        if self.safety_checker is not None:
+            safety_checker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(device)
+            image, has_nsfw_concept = self.safety_checker(
+                images=image, clip_input=safety_checker_input.pixel_values.to(dtype)
+            )
+        else:
+            has_nsfw_concept = None
+        return image, has_nsfw_concept
