be9000ff9b2b14e08bf31035c2f9245530c494cb
Jie An <pkuanjie@gmail.com>
2024-02-06 16:50:31 +0000

    up

--- debug.sh
+++ debug.sh
@@ -1,10 +1,7 @@
-rm -rf ./save
-export NCCL_DEBUG=INFO
-CUDA_VISIBLE_DEVICES="0,1,2,3" \
-accelerate launch examples/scripts/ddpo_emu1.py \
+CUDA_VISIBLE_DEVICES=0 python examples/scripts/ddpo_emu1.py \
         --num_epochs=400 \
         --train_gradient_accumulation_steps=4 \
-        --sample_num_steps=5 \
+        --sample_num_steps=50 \
         --sample_batch_size=2 \
         --train_batch_size=2 \
         --sample_num_batches_per_epoch=4 \
